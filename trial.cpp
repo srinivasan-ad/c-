dc87d53d00b31202a211984af1564868e4b78e74
srinivasan-ad <forprogramingpurpose@gmail.com>
2024-12-31 00:50:05 +0530

    hash

---
+++ trial.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+using namespace std;
+int main()
+{
+unordered_map<char,int> Val ;
+string input;
+int max = 0;
+char max_key ;
+std:: cin >> input;
+for(int i = 0 ; i < input.size() ; i++)
+{
+    Val[input[i]]++;
+    if(Val[input[i]] > max)
+    {
+        max = Val[input[i]];
+        max_key = input[i];
+    }
+}
+std :: cout<< max_key << ":" << max <<endl;
+
+}
