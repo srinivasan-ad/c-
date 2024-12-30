ca3b4891e85b9f3a1d2d8f9f79e972cc904557f7
srinivasan-ad <forprogramingpurpose@gmail.com>
2024-12-30 21:42:40 +0530

    hash

--- hash.cpp
+++ hash.cpp
@@ -3,7 +3,15 @@
 #include <unordered_map>
 using namespace std;
 int main(){
+//Creating an unordered map
 unordered_map<string,int>Val;
+//Creating pairs for insertion 
+//Type 1
 pair<string,int> p1 = make_pair("Hello",2);
 Val.insert(p1);
+// Type 2
+pair<string,int> p2("Namaste",4);
+Val.insert(p2);
+//Type 3
+Val["Champ"] = 1;
 }
