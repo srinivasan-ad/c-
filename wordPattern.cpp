aca13f0a2ad0f8867c678a775d221ac0da1d4f96
srinivasan-ad <forprogramingpurpose@gmail.com>
2024-12-31 22:36:40 +0530

    hash

--- wordPattern.cpp
+++ wordPattern.cpp
@@ -2,3 +2,36 @@
 #include <string>
 #include <unordered_map>
 using namespace std;
+    bool wordPattern(string pattern, string s) {
+       unordered_map<string,char>Val1;
+       unordered_map<char,string>Val2;
+        int flag = 0;
+        string a ;
+       for(int i = 0 ; i <= s.size() ; i++)
+       {
+         if( s[i] == ' ' || i == s.size())
+        {
+            if(flag > pattern.size()){
+                return false;
+            }
+                if((Val1.count(a) && Val1[a] != pattern[flag] )|| (Val2.count(pattern[flag]) && Val2[pattern[flag]] != a)) 
+   {
+    return false;
+       }
+            Val1[a] = pattern[flag];
+            Val2[pattern[flag]] = a;
+            a = "";
+            flag++;
+        }
+        else{
+         a += s[i];
+          }
+            }
+            if(flag != pattern.size())
+            {
+                return false;
+            }
+      
+return true;
+
+    }
