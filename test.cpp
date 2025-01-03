aff3d8f8d43eb544e3a0282e971dd6b5cef35019
srinivasan-ad <forprogramingpurpose@gmail.com>
2025-01-03 22:36:58 +0530

    test

--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <math.h>
 using namespace std ;
 void Hello( string champ){
     std:: cout<< "Hello " + champ; 
@@ -9,4 +10,11 @@ void Hello( string champ){
 int main()
 {
     Hello("champ");
+    int n = 19;
+    int num1 = n / 10;
+    int num2 = n - num1*10; 
+     std:: cout<< "n1 is : " << num1 <<endl; 
+      std:: cout<< "n2 is : " << num2 <<endl; 
+        std:: cout<< "res is : " << pow(num1,2) + pow(num2,2) <<endl; 
+     
 }
