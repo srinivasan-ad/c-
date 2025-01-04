6c0599fd24ebd71f4609210838639710ff5e36cb
srinivasan-ad <forprogramingpurpose@gmail.com>
2025-01-04 10:10:14 +0530

    patterns

--- Patterns/square.cpp
+++ Patterns/square.cpp
@@ -1,14 +1,33 @@
+// #include <iostream>
+// int main()
+// {
+//     int n ; 
+//     int num = 1;
+//     std :: cout << "Enter val of n ->" << std:: endl ;
+//     std :: cin >> n ;
+//     for(int i = 1 ; i <= n*n ; i ++ )
+//     {
+//         std :: cout << num << " " ;
+//         num++;
+//         if(i%n == 0)
+//         {
+//             std :: cout << std:: endl;
+//         }
+      
+//     }
+
+// }
 #include <iostream>
 int main()
 {
     int n ; 
-    int num = 1;
+    char alpha = 'A';
     std :: cout << "Enter val of n ->" << std:: endl ;
     std :: cin >> n ;
     for(int i = 1 ; i <= n*n ; i ++ )
     {
-        std :: cout << num << " " ;
-        num++;
+        std :: cout << alpha << " " ;
+        alpha++;
         if(i%n == 0)
         {
             std :: cout << std:: endl;
