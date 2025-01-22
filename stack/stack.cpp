e60475c06fb8fbf23fc527aebf8530e1e81c95ec
srinivasan-ad <forprogramingpurpose@gmail.com>
2025-01-22 22:09:13 +0530

    stack

--- stack/stack.cpp
+++ stack/stack.cpp
@@ -2,14 +2,25 @@
 #include<stack>
 int main(){
     std :: cout << "Using stack" << std ::  endl;
-    std :: stack<int>Val;
+    std :: stack<int>val;
     //empty() function and size() funnction
-    if(Val.empty())
+    if(val.empty())
     {
         std :: cout << "stack is empty !"  << std ::  endl;
     }
     else{
-        std :: cout << " stack is not empty  "  << std :: endl;
+        std :: cout << " stack is not empty  " << " " << val.size()  << std :: endl;
     }
     //push() operation
+    val.push(1);
+    val.push(2);
+    val.push(3);
+    val.push(4);
+    val.push(5);
+    val.push(6);
+       std :: cout << " stack is not empty  " << " " << val.size()  << std :: endl;
+val.pop();
+val.pop();
+   std :: cout << " stack is not empty  " << " " + val.size()  << std :: endl;
+    
 }
