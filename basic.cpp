2c43afc6812d0eef29f82936630c154d546feff9
srinivasan-ad <forprogramingpurpose@gmail.com>
2025-01-24 13:49:04 +0530

    basic

--- basic.cpp
+++ basic.cpp
@@ -14,49 +14,51 @@ int main()
     std :: string s = "acBbed";
     array<int,4> arr;
     vector<int> vec;
-    // vec[4] = 5;
-    // cout << vec[4] << endl;
+    vec[4] = 5;
+    cout << vec[4] << endl;
     vec.push_back(7);
     vec.push_back(6);
     vec.push_back(10);
     vec.push_back(9);
-    // cout << vec[0]<<endl;
-    // for(int i = 0 ; i < vec.size() ; i++)
-    // {
-    //     cout << vec[i] << endl;
-    // }
-    // for(auto num : vec)
-    // {
-    //     cout << num << endl;
-    // }
-    // vec.pop_back();
-    // for(auto num : vec)
-    // {
-    //     cout << num << endl;
-    // }
-    // cout << "sorted series : ";
-    // sort(vec.begin() , vec.end());
-    // for(auto num : vec)
-    // {
-    //     cout << num << endl;
-    // }
-    // cout << "reverse series : " ;
-    // reverse(vec.begin()+1 , vec.end()-1);
-    // for(auto num : vec)
-    // {
-    //     cout << num << endl;
-    // }
-//    sort(s.begin(),s.end());
-//    for(auto c : s)
-//     {
-//         cout << c << endl;
-//     }
+    cout << vec[0]<<endl;
+    for(int i = 0 ; i < vec.size() ; i++)
+    {
+        cout << vec[i] << endl;
+    }
+    for(auto num : vec)
+    {
+        cout << num << endl;
+    }
+    vec.pop_back();
+    for(auto num : vec)
+    {
+        cout << num << endl;
+    }
+    cout << "sorted series : ";
+    sort(vec.begin() , vec.end());
+    for(auto num : vec)
+    {
+        cout << num << endl;
+    }
+    cout << "reverse series : " ;
+    reverse(vec.begin()+1 , vec.end()-1);
+    for(auto num : vec)
+    {
+        cout << num << endl;
+    }
+   sort(s.begin(),s.end());
+   for(auto c : s)
+    {
+        cout << c << endl;
+    }
      int num = 60 - '0';
      cout << num << endl;
-     vector<vector<int>> vec2;
+     vector<vector<int>> vec2(1);
      vec2.push_back(vec);
      vec.push_back(9);
      vec2.push_back(vec);
+     vec2.push_back({1,2,3,4});
+    vec2[0] = {7,8,9,10};
      for(int i = 0 ; i < vec2.size() ; i++)
      {
         for(int j = 0 ; j < vec2[i].size() ; j++)
@@ -65,6 +67,4 @@ int main()
         }
               cout << "" <<endl;
      }
-
-
 }
