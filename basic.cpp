0f5a8f09931b0a3bf90bbac4e86b8d738527c24e
srinivasan-ad <forprogramingpurpose@gmail.com>
2025-01-24 13:03:29 +0530

    basic

--- basic.cpp
+++ basic.cpp
@@ -1,1 +1,12 @@
 #include <iostream>
+#include <string>
+int main()
+{
+    int i = 0;
+    float j  =  0.0 ;
+    double k = 0.00;
+    long j = 0;
+    char c = 'a';
+    std :: string s = "a"; 
+
+}
