6a6d49ffd8e4ce4f3e12c8424ca54a8ce301a549
srinivasan-ad <forprogramingpurpose@gmail.com>
2025-03-12 20:55:47 +0530

    cool :zap:

--- linkedList/linkedList.cpp
+++ linkedList/linkedList.cpp
@@ -1,17 +1,86 @@
 //singly linked list 
+// #include <iostream>
+// using namespace std;
+// class node
+// {
+// public:
+//     int data;
+//     node *next;
+//     node(int val)
+//     {
+//         data = val;
+//         next = NULL;
+//     }
+// };
+// void insertAtEnd(node *&head, int val)
+// {
+//     node *n = new node(val);
+//     if (head == NULL)
+//     {
+//         head = n;
+//         return;
+//     }
+//     node *temp = head;
+//     while (temp->next != NULL)
+//     {
+//         temp = temp->next;
+//     }
+//     temp->next = n;
+// }
+
+// void insertAtHead(node* &head , int val)
+// {
+// node* n = new node(val);
+// if(head == NULL)
+// {
+//     head = n ;
+//     return;
+
+// }
+// node* temp = head;
+// n -> next = temp;
+// head = n;
+// }
+// void display(node *&head)
+// {
+//     node *temp = head;
+//     while (temp != NULL)
+//     {
+
+//         cout << temp->data << " " << "->" << " ";
+
+//         temp = temp->next;
+//     }
+//     cout << "NULL" << endl;
+// }
+// int main()
+// {
+//     node *head = NULL;
+//     insertAtEnd(head, 0);
+//     insertAtEnd(head, 1);
+//     insertAtHead(head,3);
+//     display(head);
+//     cout << "hello";
+// }
+//doubly linked list
 #include <iostream>
 using namespace std;
 class node
 {
 public:
+
     int data;
+    node *prev;
     node *next;
     node(int val)
     {
         data = val;
+        prev = NULL;
         next = NULL;
+      
     }
 };
+
 void insertAtEnd(node *&head, int val)
 {
     node *n = new node(val);
@@ -26,8 +95,8 @@ void insertAtEnd(node *&head, int val)
         temp = temp->next;
     }
     temp->next = n;
+    n -> prev = temp;
 }
-
 void insertAtHead(node* &head , int val)
 {
 node* n = new node(val);
@@ -39,6 +108,7 @@ if(head == NULL)
 }
 node* temp = head;
 n -> next = temp;
+temp -> prev = n;
 head = n;
 }
 void display(node *&head)
@@ -60,5 +130,5 @@ int main()
     insertAtEnd(head, 1);
     insertAtHead(head,3);
     display(head);
-    cout << "hello";
+    cout << "wow";
 }
