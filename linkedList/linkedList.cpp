57e7846cb7bfb5b6534f587c69993e73c7b82630
srinivasan-ad <forprogramingpurpose@gmail.com>
2025-03-13 07:55:27 +0530

    cool :zap:

--- linkedList/linkedList.cpp
+++ linkedList/linkedList.cpp
@@ -111,6 +111,53 @@ n -> next = temp;
 temp -> prev = n;
 head = n;
 }
+int listLen(node* &head)
+{
+int len = 0;
+node* temp = head;
+while(temp != NULL)
+{
+    len++;
+    temp = temp -> next;
+}    
+return len;
+}
+void insertAtAnyPosition(node* &head , int val , int pos)
+{
+    int len = listLen(head);
+    if(pos > len + 1 || pos < 1)
+    {
+        cout << "Position entered is invalid :(" << endl;
+        return;
+    }
+    if(pos == 1)
+    {
+        insertAtHead(head , val);
+        return;
+    }
+    if(pos == len + 1)  
+    {
+        insertAtEnd(head , val);
+        return;
+    }
+
+    node* n = new node(val);
+    node* temp = head;
+    for(int count = 1; count < pos - 1; count++) 
+    {
+        temp = temp->next;
+    }
+    
+    node* nextNode = temp->next;
+    
+    temp->next = n;
+    n->prev = temp;
+    n->next = nextNode;
+    
+    if (nextNode != NULL)
+        nextNode->prev = n;
+}
+
 void display(node *&head)
 {
     node *temp = head;
@@ -122,13 +169,15 @@ void display(node *&head)
         temp = temp->next;
     }
     cout << "NULL" << endl;
+
 }
 int main()
 {
     node *head = NULL;
-    insertAtEnd(head, 0);
-    insertAtEnd(head, 1);
-    insertAtHead(head,3);
+insertAtAnyPosition(head , 1 , 5);
+    display(head);
+    insertAtAnyPosition(head , 4, 2);
+ insertAtAnyPosition(head,3,1);
     display(head);
     cout << "wow";
 }
