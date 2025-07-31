cafb3da766fb0d39238569e7979c0eebcdc3dd84
srinivasan-ad <forprogramingpurpose@gmail.com>
2025-07-31 12:44:51 +0530

    recurssion

--- Recurssion/p8.cpp
+++ Recurssion/p8.cpp
@@ -7,10 +7,24 @@ void Print(int n)
     Print(n/10);
     cout << n%10 << " ";
 }
+
+int Armstrong(int n)
+{
+    if(n == 0) return 0;
+    int last = n%10;
+    return last*last*last + Armstrong(n/10);
+}
 int main()
 {
   int n;
   cout << "Enter the value of n " << endl;
   cin >> n;
   Print(n);
+  cout << endl;
+  if(n == Armstrong(n)) 
+  {
+    cout << n << " is an Armstrong number !" << endl;
+    return 0;
+  }
+  cout << n << " is not an Armstring number " << endl;
 }
