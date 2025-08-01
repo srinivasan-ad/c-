18947de4bcd42e56ea57f555f567aef8cc28d4aa
srinivasan-ad <forprogramingpurpose@gmail.com>
2025-08-02 00:20:33 +0530

    Arrays/vectors

--- vector/grind/p2.cpp
+++ vector/grind/p2.cpp
@@ -2,7 +2,8 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-int Second(vector<int> arr)
+//Using loop
+int Second(vector<int>& arr)
 {
     int max1 = arr[0];
     int max2 = INT_MIN;
@@ -21,9 +22,25 @@ int Second(vector<int> arr)
     }
     return max2;
 }
+//Using recurssion
+int Second2(vector<int>& arr , int cur , int  max1 , int max2)
+{
+    if(cur == arr.size()) return max2;
+  if(max1 < arr[cur])
+        {
+            max2 = max1;
+            max1 = arr[cur];
+            
+        }
+        else if(arr[cur] > max2 && arr[cur] < max1)
+        {
+            max2 = arr[cur];
+        }   
+        return Second2(arr, cur + 1, max1 , max2);
+}
 int main()
 {
  vector<int> arr ={10,20};
- int  max2 = Second(arr);
+ int  max2 = Second2(arr,0 , arr[0] , INT_MIN);
  cout << "Second Largest number is -> " << max2 << endl;
 }
