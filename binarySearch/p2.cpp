20367f80ecf454cf66b94bc86d3b04a723fc2cff
srinivasan-ad <forprogramingpurpose@gmail.com>
2025-08-15 20:02:54 +0530

    Started with binary search

--- binarySearch/p2.cpp
+++ binarySearch/p2.cpp
@@ -1,18 +1,52 @@
 // Lower bound an Upper bound
+// Lower bound returns the first index where k can be inserted to keep the array sorted 
+// Upper bound returns the first index greater than k 
 #include<iostream>
 #include<vector>
 using namespace std;
-int lowerBound(vector<int> arr , int k)
+// Worked by luck
+// int lowerBound(vector<int> arr , int k)
+// {
+//     int low = 0;
+//     int high = arr.size()-1;
+//     int mid;
+//     while(low <= high)
+//     {
+//         mid = low + (high-low)/2;
+//         if(arr[mid-1] < k && arr[mid + 1] > k)
+//         {
+//             return mid;
+//         }
+//         else if(k > arr[mid])
+//         {
+//             low = mid + 1;
+//         }
+//         else
+//         {
+//             high = mid -1;
+//         }
+//     }
+//     if(arr[mid] >= k)
+//     {
+//         return mid;
+//     }
+//     return -1;
+// }
+
+// Actual lower bound 
+ int lowerBound(vector<int> arr , int k)
 {
     int low = 0;
     int high = arr.size()-1;
-    int mid;
+    int mid = arr.size()-1;
+     int ans;
     while(low <= high)
     {
         mid = low + (high-low)/2;
-        if(arr[mid-1] < k)
+        if(arr[mid] >= k)
         {
-            return mid;
+           ans = mid;
+           high = mid - 1;
         }
         else if(k > arr[mid])
         {
@@ -23,13 +57,44 @@ int lowerBound(vector<int> arr , int k)
             high = mid -1;
         }
     }
-    if(arr[mid] == k)
-    {
-        return mid;
-    }
-    return -1;
+    
+    return ans;
 }
+//upperbound
+ int upperBound(vector<int> arr , int k)
+{
+    int low = 0;
+    int high = arr.size()-1;
+    int mid = arr.size()-1;
+     int ans = arr[arr.size()-1]+1 ;
+    while(low <= high)
+    {
+        mid = low + (high-low)/2;
+        if(arr[mid] > k)
+        {
+           ans = mid;
+           high = mid - 1;
+        }
+        else 
+        {
+            low = mid + 1;
+        }
+    }
+    
+    return ans;
+}
+
+
+
 int main()
 {
+  vector<int> arr = {1,2,3,4,5,6,7,9,9,9,9,9,10};
+   int k = 7;
+
+   int ans = lowerBound(arr,k);
+   int ans2 = upperBound(arr,k);
+    cout << "Lower Bound of " << k << " found at index -> " << ans << endl;
+     cout << "Upper Bound of " << k << " found at index -> " << ans2 << endl;
+
 
 }
